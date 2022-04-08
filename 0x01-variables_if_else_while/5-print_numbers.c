7e805348d17a6c406d91bfe60687d50fd38a8475
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-08 15:55:08 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	
+	while (a < 10)
+	{
+		printf("%i", a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
