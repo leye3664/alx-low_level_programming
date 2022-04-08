e4477f34b2870123eff16f99ffeb430426684639
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-08 14:21:14 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char j = 'a';
+
+	while (j <= 'z')
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
