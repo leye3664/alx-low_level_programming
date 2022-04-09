ed266a5762f57701f3e3d4d153226c7f4c813875
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-08 17:23:37 -0700

    NSG

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+
+	int a, b, c, d;
+
+	a = 0;
+
+	while (a < 1000)
+	{
+		b = a % 10;
+		c = (a / 10) % 10;
+		d = a / 100;
+
+		if (b > c && c > d)
+		{
+			putchar(d + '0');
+			putchar(c + '0');
+			putchar(b + '0');
+
+			if (a < 789)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
+
