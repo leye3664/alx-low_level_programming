ae84dadb534ae7f1c083df5103abc6afe11c57a4
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-13 15:26:41 -0700

    f

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of a number
+ * @n: number to be checked
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
