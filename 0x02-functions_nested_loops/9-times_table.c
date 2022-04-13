2a5059179c1aa160a03de20c418e719f821f4f2d
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-13 16:25:58 -0700

    p

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#inlcude "main.h"
+/**
+ * times_table - prints 9 times table
+ */
+void times_table(void)
+{
+	int a = 0, j, k;
+
+	while (a < k)
+	{
+		k = a * j;
+
+		if (k <= 9 && j != 0)
+		{
+			_putchar(32);
+			_putchar(k + '0');
+		}
+		else if (j == 0)
+		{
+			_putchar(k + '0');
+		}
+		else
+		{
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+		}
+		if (j < 9)
+		{
+			_putchar(44);
+			_putchar(32);
+		}
+		j++;
+	}
+	_putchar('\n');
+	a++;
+}
