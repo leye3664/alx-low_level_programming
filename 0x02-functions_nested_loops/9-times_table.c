50c34167b27a42be57e33775666617bddcaacf91
olumide <85311688+leye3664@users.noreply.github.com>
2022-04-14 00:32:58 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,37 @@
 void times_table(void)
 {
 	int a = 0, j, k;
-
-	while (a < k)
+	
+	while (a < 10)
 	{
-		k = a * j;
+		j = 0;
 
-		if (k <= 9 && j != 0)
+		while (a < k)
 		{
-			_putchar(32);
-			_putchar(k + '0');
+			k = a * j;
+
+			if (k <= 9 && j != 0)
+			{
+				_putchar(32);
+				_putchar(k + '0');
+			}
+			else if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+			j++;
 		}
-		else if (j == 0)
-		{
-			_putchar(k + '0');
-		}
-		else
-		{
-			_putchar(k / 10 + '0');
-			_putchar(k % 10 + '0');
-		}
-		if (j < 9)
-		{
-			_putchar(44);
-			_putchar(32);
-		}
-		j++;
+		_putchar('\n');
+		a++;
 	}
-	_putchar('\n');
-	a++;
 }
