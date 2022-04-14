383a016fdfeda917647060aa4b3428073f588810
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-13 17:12:27 -0700

    solace

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * Return: 0
+ */
+int main(void)
+{
+	long int a = 1, b = 2, n = 0, d, k = 2;
+
+	while (n < 50)
+	{
+		d = a + b;
+		if (d % 2 == 0 && d <= 4000000)
+		{
+			k += d;
+		}
+		a = b;
+		b = d;
+		n++;
+	}
+	printf("%lu\n", k);
+	return (0);
+}
+
+	   
