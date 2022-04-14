4bdf74e523d5a309313720ffbec4d71d2cbc8270
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-14 14:36:36 -0700

    g

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - printsa straight line
+ * @n: how long line would be
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar(95);
+	}
+	_putchar(10);
+}
