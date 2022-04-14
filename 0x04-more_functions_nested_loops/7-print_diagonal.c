3d9f37ad3656da2d9b3ae055bc9d2facd15fb2e2
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-14 15:03:48 -0700

    b

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - print a diagonal
+ * @n: how long line would be
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		_putchar(92);
+		if (n > 1)
+		{
+			for (i = 1; i < n; i++)
+			{
+				_putchar(10);
+				j = i;
+
+				while (j != 0)
+				{
+					_putchar(32);
+					j--;
+				}
+				_putchar(92);
+			}
+		}
+	}
+	_putchar(10);
+}
