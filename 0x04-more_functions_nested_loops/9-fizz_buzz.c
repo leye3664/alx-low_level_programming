4585d880738276258ef94e002e87b5228b20fd12
olumide <85311688+leye3664@users.noreply.github.com>
2022-04-15 01:06:08 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return: 0
- */
+* main - main block
+* Return: 0
+*/
 int main(void)
 {
-	int i = 1
-
-		while (i < 101)
+	int i = 1;
+	
+	while (i < 101)
+	{
+		if (i % 15 == 0)
 		{
-			if (i % 15 == 0)
-			{
-				printf("FizzBuzz ");
-			}
-			else if (i % 5 == 0 && i < 100)
-			{
-				printf("Buzz ");
-			}
-			else if (i == 100)
-			{
-				printf("Buzz");
-			}
-			else if (i % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else
-			{
-				printf("%i ", i);
-			}
-			i++;
+			printf("FizzBuzz ");
 		}
+		else if (i % 5 == 0 && i < 100)
+		{
+			printf("Buzz ");
+		}
+		else if (i == 100)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+		i++;
+	}
 	printf("\n");
 	return (0);
 }
