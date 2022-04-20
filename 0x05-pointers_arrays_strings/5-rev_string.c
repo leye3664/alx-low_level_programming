fa26dea0fdd33342e20c1beb58d6a51c8044e957
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-20 15:34:32 -0700

    amber

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: char type pointer 
+ */
+void rev_string(char *s)
+{
+	int i = 0, j, l = 0;
+	char k;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	j = i;
+	while (l <= j / 2)
+	{
+		k = s[l];
+		s[l] = s[i];
+		s[i] = k;
+		i--;
+		l++;
+	}
+}
