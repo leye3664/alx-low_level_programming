f5096e798b2e443ae3cc9b3e0da17c40e8eb5282
“leye3664” <oluleyeolawale2505@gmail.com>
2022-04-20 15:48:53 -0700

    babe

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of  a string
+ * @str: char type pointer
+ */
+void puts(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i--;
+	j = i;
+	for (i = 0; i <= j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
