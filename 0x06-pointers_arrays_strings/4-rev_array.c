e83ae89021cb163f64ecb1a06ea1e858972ef26a
olumide <85311688+leye3664@users.noreply.github.com>
2022-04-24 22:31:46 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,7 +4,7 @@
  * @a: int type pointer 
  * @n: number of elements of the array
  */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
 	int tep, beg = 0;
 	int end = n - 1;
