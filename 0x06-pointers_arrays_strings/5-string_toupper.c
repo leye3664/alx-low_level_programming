d5b68c85b5211654505978bfa26daf044f94bcb4
olumide <85311688+leye3664@users.noreply.github.com>
2022-04-24 22:20:06 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
- * @srt: char type pointer
+ * @str: char type pointer
  * Return: the updated string
  */
 char *string_toupper(char *str)
